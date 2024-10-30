313b67685d699217086f8a82fd3d0d923651a112
VDRBreno <vdr.breno0149@outlook.com>
2024-10-30 17:49:35 -0300

    Create: product entity

---
+++ src/entities/Product.ts
@@ -0,0 +1,28 @@
+import { randomUUID } from 'node:crypto';
+
+export default class Product {
+
+  public readonly id: string;
+  
+  public name: string;
+  public description: string;
+  public image_name: string;
+  public value: number;
+  public quantity: number;
+  
+  public created_at: Date;
+
+  constructor(
+    props: Omit<Product, 'id' | 'created_at'>,
+    optionals?: { id: Product['id']; created_at: Product['created_at']; }
+  ) {
+    this.id = optionals?.id ?? randomUUID();
+    this.name = props.name;
+    this.description = props.description;
+    this.image_name = props.image_name;
+    this.value = props.value;
+    this.quantity = props.quantity;
+    this.created_at = optionals?.created_at ?? new Date()
+  }
+
+}
