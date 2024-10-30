a7992fffe35e011d7ec192ded918267189adf263
VDRBreno <vdr.breno0149@outlook.com>
2024-10-30 18:50:48 -0300

    Add: /user/create and /user/auth in routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,8 +3,13 @@ import { Router } from 'express';
 import { multerUpload } from '@/config/multer.config';
 
 import CreateProductController from './Product/Create';
+import CreateUserController from './User/Create';
+import AuthUserController from './User/Auth';
 
 const routes = Router();
+
+routes.post('/user/create', CreateUserController.handler);
+routes.post('/user/auth', AuthUserController.handler);
 
 routes.post('/product/create', multerUpload.single('image'), CreateProductController.handler);
 
