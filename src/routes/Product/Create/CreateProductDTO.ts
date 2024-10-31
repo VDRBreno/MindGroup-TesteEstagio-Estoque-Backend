4e39cd7260d2f6fffe6b7b604bada718d1d159a2
VDRBreno <vdr.breno0149@outlook.com>
2024-10-31 14:41:08 -0300

    Change: just change order of lines

--- src/routes/Product/Create/CreateProductDTO.ts
+++ src/routes/Product/Create/CreateProductDTO.ts
@@ -3,24 +3,26 @@ import Joi from 'joi';
 import RequestDTO from '@/entities/RequestDTO';
 
 export interface ICreateProductRequestDTO {
-  name: string;
-  description: string;
-  value: string;
   file: {
     filename: string;
   };
+
+  name: string;
+  description: string;
+  value: string;
 }
 
 export default class CreateProductRequestDTO extends RequestDTO<ICreateProductRequestDTO> {
 
   validate(data: unknown) {
     const schema = Joi.object({
-      name: Joi.string().required(),
-      description: Joi.string().required(),
-      value: Joi.number().required(),
       file: Joi.object({
         filename: Joi.string().required()
-      }).required()
+      }).required(),
+
+      name: Joi.string().required(),
+      description: Joi.string().required(),
+      value: Joi.number().required()
     });
 
     const result = schema.validate(data);
