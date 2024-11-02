4126dd9197a625264b0fc93d3c301cf99ea9e7f3
VDRBreno <vdr.breno0149@outlook.com>
2024-11-02 08:03:25 -0300

    Change: /uploads directory to /images, remove /uploads-raw + Create: keys directory

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -2,5 +2,7 @@ import path from 'node:path';
 
 export const SRC_DIRECTORY = path.join(__dirname, '..');
 export const ROOT_DIRECTORY = path.join(SRC_DIRECTORY, '..');
-export const UPLOADS_DIRECTORY = path.join(ROOT_DIRECTORY, 'uploads');
-export const UPLOADS_RAW_DIRECTORY = path.join(ROOT_DIRECTORY, 'uploads-raw');
+export const UPLOAD_IMAGES_DIRECTORY = path.join(ROOT_DIRECTORY, 'images');
+export const KEYS_DIRECTORY = path.join(ROOT_DIRECTORY, 'keys');
+export const KEY_JWT_PRIVATE_PATH = path.join(KEYS_DIRECTORY, 'jwt_private.key');
+export const KEY_JWT_PUBLIC_PATH = path.join(KEYS_DIRECTORY, 'jwt_public.key');
