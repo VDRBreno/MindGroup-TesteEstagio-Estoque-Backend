d570fdf6c739255472a7a9f3620124a8e827547b
VDRBreno <vdr.breno0149@outlook.com>
2024-11-02 18:10:33 -0300

    Fix: change server host from 127.0.0.1 to 0.0.0.0

--- src/config/server.config.ts
+++ src/config/server.config.ts
@@ -2,6 +2,6 @@ import getEnvOrDefault from '@/utils/getEnvOrDefault';
 
 export const SERVER_CONFIG = {
   port: getEnvOrDefault('PORT', 3333),
-  host: getEnvOrDefault('HOST', '127.0.0.1')
+  host: getEnvOrDefault('HOST', '0.0.0.0')
 }
 export const SERVER_URL = `${SERVER_CONFIG.host}:${SERVER_CONFIG.port}`;
